4cd00ca79e797f4297ad9b970a291de38bcf45dc
Jerry Huang <q89662300q@gmail.com>
2024-05-28 16:10:05 +0800

    add_JoinList

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,6 +37,12 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Joinlist()
+    {
+        return View();
+    }
+
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
