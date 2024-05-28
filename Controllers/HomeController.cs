fbb558178c780a617251442eec0eeb5771175763
Jerry Huang <q89662300q@gmail.com>
2024-05-28 20:22:44 +0800

    調整架構+Add門禁系統

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -42,6 +42,15 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Accessdoor()
+    {
+        return View();
+    }
+
+    public IActionResult Doorsystem()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
